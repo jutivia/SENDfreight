65d01a87e16d6c0cf13b8229559b7ad2a7f17f73
user <user@users-MacBook-Air.local>
2021-10-09 23:01:50 +0100

    initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <Header>
+            <div className="lhs">
+                <h4>SEND<span>FREIGHT</span></h4>
+                <div className="search">
+                <input type="search" name="Search" id="Search" placeholder="Search"/>
+                </div>
+            </div>
+        </Header>
+    )
+}
+
+export default Header
